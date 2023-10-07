3e87757cc09d85ce04b5f058f48985b7a6c1351c
PeterVanhoolst <peter.vanhoolst@gmail.com>
2023-10-07 08:32:55 +0200

    serie gelinkt aan locatie

---
+++ andornot.mjs
@@ -0,0 +1,8 @@
+let totaalAantalPunten = 125;
+let aantalStudenten = 10;
+let jeScore = 13;
+let minimumVoorOnderscheiding = 14;
+
+if((totaalAantalPunten / aantalStudenten) <= jeScore && jeScore >= minimumVoorOnderscheiding){
+    console.log("true)")};
+process.exit()
